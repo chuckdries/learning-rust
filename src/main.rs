923861f52c940966562c18ac958e257cc39f7623
Chuck Dries <chuck@chuckdries.com>
2018-11-30 21:48:28 -0800

    prompt user for guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    print!("Please input your guess> ");
+
+    io::stdout().flush()
+        .expect("Flush failed?");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line for some reason");
+
+    println!("You guessed {}", guess);
 }
