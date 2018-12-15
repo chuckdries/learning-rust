34cb7b9750f1f602a94b001eff863addf62fcc5f
Chuck Dries <chuck@chuckdries.com>
2018-12-15 15:43:01 -0700

    exercise 1 clone solution
    fix ex1 clone solution

--- crash-course/lesson-1/src/main.rs
+++ crash-course/lesson-1/src/main.rs
@@ -1,20 +1,9 @@
-use std::fmt;
-
-struct Person {
-    name: String,
-    age: u32,
-}
-
-impl fmt::Display for Person {
-    fn fmt(self: &Self, fmt: &mut fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(fmt, "{} ({} years old)", self.name, self.age)
-    }
-}
-
 fn main() {
-    let alice = Person {
-        name: String::from("Alice"),
-        age: 30,
-    };
-    println!("Hello, {}!", alice);
+    let val: String = String::from("Hello, world!");
+    printer(val.clone());
+    printer(val);
+}
+
+fn printer(value: String) -> () {
+    println!("value: {}", value);
 }
