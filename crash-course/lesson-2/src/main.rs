9524bf8c0a1c6ebe803606bc5c5d7d08ec6b2282
Chuck Dries <chuck@chuckdries.com>
2018-12-15 16:30:21 -0700

    "reference outlives value" question

--- crash-course/lesson-2/src/main.rs
+++ crash-course/lesson-2/src/main.rs
@@ -7,21 +7,16 @@ impl Drop for Foobar {
     }
 }
 
-impl Foobar {
-    fn use_it(&self) {
-        println!("I consumed myself, a Foobar: {:?}", self);
-    }
-}
-
 fn uses_foobar(foobar: &Foobar) {
     println!("I consumed a Foobar: {:?}", foobar);
 }
 
 fn main() {
-    let x = Foobar(1);
+    let x: Foobar = Foobar(1);
+    let y: &Foobar = &x;
     println!("Before uses_foobar");
-    x.use_it();
     uses_foobar(&x);
-    uses_foobar(&x);
+    uses_foobar(y); // why does this call work with and without the &?
+    uses_foobar(&y); // why does this still work after y was consumed?
     println!("After uses_foobar");
 }
