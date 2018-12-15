8b004a87536d2b2dca8ec30816101e9c73957d46
Chuck Dries <chuck@chuckdries.com>
2018-12-15 16:18:44 -0700

    challenge: lexical scoping braces

--- crash-course/lesson-2/src/main.rs
+++ crash-course/lesson-2/src/main.rs
@@ -7,17 +7,14 @@ impl Drop for Foobar {
     }
 }
 
-fn drop_foobar(_foobar: Foobar) {}
-
-fn uses_foobar(foobar: &Foobar) {
-    println!("I consumed a Foobar: {:?}", foobar);
-}
-
 fn main() {
-    let x = Foobar(1);
-    println!("Before uses_foobar");
-    uses_foobar(&x);
-    println!("After uses_foobar");
-    drop_foobar(x);
-    println!("After drop_foobar");
+    println!("Before x");
+    let _x = Foobar(1);
+    println!("After x");
+//    {
+        println!("Before y");
+        let _y = Foobar(2);
+        println!("After y");
+//    }
+    println!("End of main");
 }
