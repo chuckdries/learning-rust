2089fc24e164f31fb88f2072d9e69e47c14ec870
Chuck Dries <chuck@chuckdries.com>
2018-12-15 16:47:08 -0700

    challenge: Mutable reference with other references

--- crash-course/lesson-2/src/main.rs
+++ crash-course/lesson-2/src/main.rs
@@ -1,22 +1,32 @@
 #[derive(Debug)]
 struct Foobar(i32);
 
-impl Drop for Foobar {
-    fn drop(&mut self) {
-        println!("Dropping a Foobar: {:?}", self);
-    }
-}
-
-fn uses_foobar(foobar: &Foobar) {
-    println!("I consumed a Foobar: {:?}", foobar);
-}
-
 fn main() {
-    let x: Foobar = Foobar(1);
-    let y: &Foobar = &x;
-    println!("Before uses_foobar");
-    uses_foobar(&x);
-    uses_foobar(y); // why does this call work with and without the &?
-    uses_foobar(&y); // why does this still work after y was consumed?
-    println!("After uses_foobar");
+    let x = Foobar(1);
+
+    foo(x);
+    //foo(x);
+
+    let mut y = Foobar(2);
+
+    bar(&y);
+    bar(&y);
+
+    bar(&y);
+    let z = &mut y;
+
+    //baz(&mut y);
+    baz(z);
+}
+
+// move
+fn foo(_foobar: Foobar) {
+}
+
+// read only reference
+fn bar(_foobar: &Foobar) {
+}
+
+// mutable reference
+fn baz(_foobar: &mut Foobar) {
 }
