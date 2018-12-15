1f75021971c013cea4d7289191e80364e0b9d0ee
Chuck Dries <chuck@chuckdries.com>
2018-12-15 16:01:58 -0700

    ex4 fizzbuzz match

--- crash-course/lesson-1/src/main.rs
+++ crash-course/lesson-1/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
     for i in 1..101 {
-        if i % 3 == 0 && i % 5 == 0 {
-            println!("fizzbuzz")
-        } else if i % 3 == 0 {
-            println!("fizz")
-        } else if i % 5 == 0 {
-            println!("buzz")
-        } else {
-            println!("{}", i)
+        match (i % 3 == 0, i % 5 == 0) {
+            (true, true) => println!("fizzbuzz"),
+            (true, false) => println!("fizz"),
+            (false, true) => println!("buzz"),
+            (false, false) => println!("{}", i),
         }
     }
 }
