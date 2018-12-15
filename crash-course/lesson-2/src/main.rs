d5d614bad80714a406d6ab0622d4ed96ee7ad056
Chuck Dries <chuck@chuckdries.com>
2018-12-15 16:13:05 -0700

    ex1: drop function

---
+++ crash-course/lesson-2/src/main.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+struct Foobar(i32);
+
+impl Drop for Foobar {
+    fn drop(&mut self) {
+        println!("Dropping a Foobar: {:?}", self);
+    }
+}
+
+fn drop_foobar(_foobar: Foobar) {}
+
+//fn uses_foobar(foobar: &Foobar) {
+//    println!("I consumed a Foobar: {:?}", foobar);
+//}
+
+fn main() {
+    let x = Foobar(1);
+    drop_foobar(x);
+    println!("Before uses_foobar");
+    //uses_foobar(&x);
+    println!("After uses_foobar");
+}
