8b97e5690a648042a641a3fb337edf49b797d97b
Chuck Dries <chuck@chuckdries.com>
2018-12-15 16:15:24 -0700

    use and drop foobar
    log order of operations

--- crash-course/lesson-2/src/main.rs
+++ crash-course/lesson-2/src/main.rs
@@ -9,14 +9,15 @@ impl Drop for Foobar {
 
 fn drop_foobar(_foobar: Foobar) {}
 
-//fn uses_foobar(foobar: &Foobar) {
-//    println!("I consumed a Foobar: {:?}", foobar);
-//}
+fn uses_foobar(foobar: &Foobar) {
+    println!("I consumed a Foobar: {:?}", foobar);
+}
 
 fn main() {
     let x = Foobar(1);
-    drop_foobar(x);
     println!("Before uses_foobar");
-    //uses_foobar(&x);
+    uses_foobar(&x);
     println!("After uses_foobar");
+    drop_foobar(x);
+    println!("After drop_foobar");
 }
