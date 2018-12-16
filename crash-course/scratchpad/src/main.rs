a03ea2a419ca37c62d505ed86210f4d7884a3118
Chuck Dries <chuck@chuckdries.com>
2018-12-16 11:17:34 -0700

    l3: scratchpad: for argument loop

--- crash-course/scratchpad/src/main.rs
+++ crash-course/scratchpad/src/main.rs
@@ -2,8 +2,7 @@ use std::env::args;
 //use std::option::Option;
 
 fn main() {
-    let mut args = args();
-    while let Some(argument) = args.next() {
-        println!("{:?}",argument);
+    for arg in args() {
+        println!("{}", arg);
     }
 }
