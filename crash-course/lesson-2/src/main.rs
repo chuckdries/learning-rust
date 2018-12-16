a6d39f61f01a003bd487df459a4f44c288d5f880
Chuck Dries <chuck@chuckdries.com>
2018-12-15 17:05:28 -0700

    l2: exercise 3: copy trait implementation

--- crash-course/lesson-2/src/main.rs
+++ crash-course/lesson-2/src/main.rs
@@ -1,11 +1,13 @@
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct Foobar(i32);
 
-impl Drop for Foobar {
-    fn drop(&mut self) {
-        println!("Dropping: {:?}", self);
+impl Clone for Foobar {
+    fn clone(&self) -> Foobar {
+        Foobar(self.0)
     }
 }
+
+impl Copy for Foobar {}
 
 fn uses_foobar(foobar: Foobar) {
     println!("I consumed a Foobar: {:?}", foobar);
@@ -13,8 +15,6 @@ fn uses_foobar(foobar: Foobar) {
 
 fn main() {
     let x = Foobar(1);
-    uses_foobar(x.clone());
     uses_foobar(x);
-//    uses_foobar(x.clone());
-    println!("after!");
+    uses_foobar(x);
 }
