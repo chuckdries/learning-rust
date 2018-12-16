fc5ad46ff8e9788f55135a7c683dfcf2ab8ce6ee
Chuck Dries <chuck@chuckdries.com>
2018-12-16 11:15:05 -0700

    l3: scratchpad: while-let argument loop

--- crash-course/scratchpad/src/main.rs
+++ crash-course/scratchpad/src/main.rs
@@ -1,14 +1,9 @@
 use std::env::args;
-use std::option::Option;
+//use std::option::Option;
 
 fn main() {
     let mut args = args();
-    loop {
-        let argument = args.next();
-        let value = match argument {
-            Option::None => break,
-            Option::Some(t) => t,
-        };
-        println!("{:?}",value);
+    while let Some(argument) = args.next() {
+        println!("{:?}",argument);
     }
 }
